db382255b7f7901f9dd3c6953fb0e8b539665954
Ajay Pawar <ajaypawar.iacsd@gmail.com>
2025-04-03 15:06:45 +0530

    addition code is done

---
+++ add.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Addition{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the first number ");
+	int num1 = sc.nextInt();
+	System.out.println("Enter the second number");
+	int num2 = sc.nextInt();
+	int add = num1 + num1;
+	System.out.println("The addition is "+ add);
+
+
+
+
+	}
+
+
+
+
+}
